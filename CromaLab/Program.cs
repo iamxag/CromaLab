5897a2583f793856b39c45693abdd6b15aa3a5ea
abdulkareem <as.abdulkareem@gmail.com>
2022-01-24 19:54:38 +0530

    i have done changes in program.cs file to change message from Hello World to Welcome to dotnet

--- CromaLab/Program.cs
+++ CromaLab/Program.cs
@@ -6,7 +6,7 @@ namespace CromaLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Dotnet Training");
         }
     }
 }
